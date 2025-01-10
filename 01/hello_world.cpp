0c0c36de5f0dbb61114163636d277b929e34afcd
hzr <2290641663@qq.com>
2025-01-10 10:47:36 +0800

    hello World

---
+++ 01/hello_world.cpp
@@ -0,0 +1,13 @@
+//
+// Created by 22906 on 2025/1/10.
+//
+// 预处理指令 代码编译前的准备工作
+#include "iostream"
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!!!" << endl;
+    cout << "你好" << endl;
+    return 0;
+}
---
+++ 01/hello_world2.cpp
@@ -0,0 +1,12 @@
+//
+// Created by 22906 on 2025/1/10.
+//
+
+#include "iostream"
+using namespace std;
+
+int main()
+{
+    cout << "hello world!!\n!" << "cesef\n" << "fafa" << endl;
+    return 0;
+}
