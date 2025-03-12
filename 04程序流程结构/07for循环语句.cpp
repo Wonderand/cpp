a13acf7003c05c54af126e9d229105ed7658f5a7
hzr <2290641663@qq.com>
2025-03-12 12:03:48 +0800

    添加for循环

---
+++ 04程序流程结构/07for循环语句.cpp
@@ -0,0 +1,17 @@
+//
+// Created by 22906 on 2025/3/12.
+//
+#include "iostream"
+using namespace std;
+
+int main(){
+    system("chcp 65001");
+    //for循环
+    // 从数字0打印到数字9
+    for (int i = 0; i < 10; i++) {
+        cout << i << endl;
+    }
+
+    system("pause");
+    return 0;
+}
