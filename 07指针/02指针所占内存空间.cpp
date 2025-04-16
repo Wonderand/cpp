b9a6bfb2eafa2cfc29f9c5ceaf229100f16e6431
hzr <2290641663@qq.com>
2025-04-16 10:54:26 +0800

    更正打印输出

--- 07指针/02指针所占内存空间.cpp
+++ 07指针/02指针所占内存空间.cpp
@@ -14,9 +14,9 @@ int main(){
     // 在32位操作系统下，指针是占用4个字节空间大小，不管是什么数据类型
     // 在64位操作系统下，指针是占用8个字节空间大小，不管是什么数据类型
     cout << "sizeof int * = " << sizeof(int *) << endl;
-    cout << "sizeof int * = " << sizeof(float *) << endl;
-    cout << "sizeof int * = " << sizeof(double *) << endl;
-    cout << "sizeof int * = " << sizeof(char *) << endl;
+    cout << "sizeof float * = " << sizeof(float *) << endl;
+    cout << "sizeof double * = " << sizeof(double *) << endl;
+    cout << "sizeof char * = " << sizeof(char *) << endl;
 
     system("pause");
     return 0;
